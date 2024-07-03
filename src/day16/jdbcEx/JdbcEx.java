c94036f4c6f40ca867ae7d652cc596a7ac766b04
Lee JuHwan <seungjoon0324@gmail.com>
2024-07-03 16:59:02 +0900

    0703 memberInsert method 생성

--- src/day16/jdbcEx/JdbcEx.java
+++ src/day16/jdbcEx/JdbcEx.java
@@ -1,7 +1,9 @@
 package day16.jdbcEx;
 
+import com.mysql.cj.jdbc.result.UpdatableResultSet;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,10 +11,53 @@ import java.sql.Statement;
 public class JdbcEx {
 
   public static void main(String[] args) {
-    mySQLConnection();
+    //회원의 정보를 사용자 받는 기능 (static method로)
+    //Member 클래스
+    memberSearch();
+    memberInsert();
+    memberUpdate();
+    memberDelete();
   }
 
-  public static void mySQLConnection() {
+  private static void memberDelete() {
+  }
+
+  private static void memberUpdate() {
+  }
+
+  private static void memberInsert() {
+    String url = "jdbc:mysql://localhost:3306/employees";
+    String userName = "root";
+    String password = "seungjoon0324^^";
+    String query = "INSERT INTO member VALUES (?,?,?)";
+
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    int result = 0;
+
+    try {
+      //Class.forName("com.mysql.cj.jdbc.Driver");
+      con = DriverManager.getConnection(url, userName, password);
+      System.out.println(con);
+      pstmt = con.prepareStatement("INSERT INTO member VALUES (?,?,?)");
+      pstmt.setInt(1, 6);
+      pstmt.setString(2, "test");
+      pstmt.setString(3, "testfcase");
+
+      result = pstmt.executeUpdate();
+      if (result == 1) {
+        System.out.println("회원 정보가 입력되었습니다.");
+      } else if (result == 0) {
+        System.out.println("회원 정보 입력이 실패하였습니다.");
+      }
+      pstmt.close();
+      con.close();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static void memberSearch() {
     String url = "jdbc:mysql://localhost:3306/employees";
     String userName = "root";
     String password = "seungjoon0324^^";
