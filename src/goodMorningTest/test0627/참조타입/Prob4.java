8d6c4c66a1b5d8d1e5b3f749178fb0ae55f3aec3
Lee JuHwan <seungjoon0324@gmail.com>
2024-07-15 09:31:25 +0900

    0715 정답 잘못 기입된 거 수정

--- src/goodMorningTest/test0627/참조타입/Prob4.java
+++ src/goodMorningTest/test0627/참조타입/Prob4.java
@@ -3,6 +3,6 @@ package goodMorningTest.test0627.참조타입;
 public class Prob4 {
 
   public static void main(String[] args) {
-    System.out.println("4번 정답 : " + 1);
+    System.out.println("4번 정답 : " + 2);
   }
 }
